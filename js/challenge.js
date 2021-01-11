04179b07e734a333acfebad6fbbb3bbbceded908
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-10 16:36:01 -0800

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -49,9 +49,27 @@ function likedNum() {
     // pause the counter
     // disable all buttons except the pause button
     // the pause button should then show the text "resume."
-
 // When 'resume' is clicked, it should restart the counter and re-enable the buttons.
+
+const pause = document.getElementById("pause");
+    // attach event listener
+pause.addEventListener("click", pauseCounter)
+function pauseCounter() {
+    clearInterval(interval)
+    pause.innerText = "resume"
+    document.getElementById("button").forEach(function(a))
+}
+
 // As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
+const comment = document.getElementById("comment-input")
+comment.addEventListener("submit", function(event));
+function(event) {
+    event.preventDefault();
+    d = querySelector(".comments")
+    e = document.createElement("p")
+    e.innerText = d
+    d.appendChild(e)
+}
 
 heart.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText),c=document.querySelector(".likes"),d=void 0;if([].concat(_toConsumableArray(c.children)).map(function(a){return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');var e=parseInt(d.children[0].innerText);d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)}),pause.addEventListener("click",function(){playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),[].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause"!==a.id&&(a.disabled=!playing)})}),commentForm.addEventListener("submit",function(a){a.preventDefault();var b=this.children[0],c=b.value;b.value="";var d=document.querySelector(".comments"),e=document.createElement("p");e.innerText=c,d.appendChild(e)});
 

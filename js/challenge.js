7275113654b2b1102bb20cc98754a9f8eed2a3c1
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-11 15:06:00 -0800

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -32,17 +32,28 @@ function incrementCounter() {
 // As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
 
     // grab heart elements
-const heart = document.getElementById("heart");
+const heartButton = document.getElementById("heart");
     //attach event lisener
-heart.addEventListener("click", likedNum)
+heartButton.addEventListener("click", likedNum)
 function likedNum() {
     let a = document.getElementById("counter");
     let b = parseInt(a.innerText);
     let c = document.querySelector(".likes");
-    let d = document.createElement("li").setAttribute("data-num", b);
-    // let e = parseInt(d.children[0].innerText);
-    d.innerText = b + " has been liked " + 1 + "time";
-    c.appendChild(d)
+    // using void 0 to prevent unwanted refresh of the page
+    // let d = void 0;
+        // if ([].concat(_toconsumableArray(c.children[0])
+        //       .map(function(a) {
+        //           return parseInt(a.dataset.num)})
+        //       .include(b))
+        //     ) {
+        //     d = document.querySelector('[data-num="'+b+'"]');
+        //     e = parseInt(d.children[0].innerText);
+        //     d.innerHTML= b + " has been liked <span>" + (e+1) + "</span> times"
+        //     }
+        // else 
+            d = document.createElement("li")
+            d.innerHTML = b + " has been liked <span>1</span> time";
+            c.appendChild(d)
 }
 
 // As a user, I can pause the counter, which should
@@ -51,25 +62,30 @@ function likedNum() {
     // the pause button should then show the text "resume."
 // When 'resume' is clicked, it should restart the counter and re-enable the buttons.
 
-const pause = document.getElementById("pause");
+let pause = document.getElementById("pause");
     // attach event listener
 pause.addEventListener("click", pauseCounter)
+    // tell what happens when user clicks on pause button
 function pauseCounter() {
-    clearInterval(interval)
-    pause.innerText = "resume"
-    document.getElementById("button").forEach(function(a))
+    let playing =!0
+    let interval = timer
+    if (playing) { playing =!1, clearInterval(interval), this.innerText = "resume" }
+    else { playing =! 0, interval = timer, this.innerText = "pause" };
 }
+
 
 // As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
-const comment = document.getElementById("comment-input")
-comment.addEventListener("submit", function(event));
-function(event) {
-    event.preventDefault();
-    d = querySelector(".comments")
-    e = document.createElement("p")
-    e.innerText = d
-    d.appendChild(e)
-}
 
-heart.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText),c=document.querySelector(".likes"),d=void 0;if([].concat(_toConsumableArray(c.children)).map(function(a){return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');var e=parseInt(d.children[0].innerText);d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)}),pause.addEventListener("click",function(){playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),[].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause"!==a.id&&(a.disabled=!playing)})}),commentForm.addEventListener("submit",function(a){a.preventDefault();var b=this.children[0],c=b.value;b.value="";var d=document.querySelector(".comments"),e=document.createElement("p");e.innerText=c,d.appendChild(e)});
+const comment = document.getElementsByTagName("form")
+    // add event listener
+comment.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let b = this.children[0]
+    let c = b.value
+    b.value = ""
+    let d = querySelector(".comments")
+    let e = document.createElement("p")
+    e.innerText = c
+    d.appendChild(e)
+})
 
